7f1e2c72e8ab03916ed1463c8d5a87983de1e2da
Fran <87648099+fkroflin@users.noreply.github.com>
2025-04-28 12:06:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 // Poslužuje sve statične datoteke iz mape public
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Osnovni endpoint
 app.get('/', (req, res) => {
